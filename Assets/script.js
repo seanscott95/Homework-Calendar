adefb1c6733d84b770f21eb94a19a0b1f3745ab9
Sean <seanscott95@live.com>
2021-12-20 01:12:06 +1100

    added pseudocode ready to plan for actual code

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,14 @@
+// Display current day and date using moment
+// Each hour must have a respepctive colour depending on the hour
+// If the hour has past display time as grey, if else it is hte current hour display red, else display green
+// Save button saves textarea to localStorage
+
+
+// Displays current day and time in - Monday, December 12th - format
 var currentDay = moment().format("dddd, MMMM Mo");
 $("#currentDay").text(currentDay);
+
+// Save button saves textarea to localStorage
+
+// If the hour has past display time as grey, if else it is hte current hour display red, else display green
+
